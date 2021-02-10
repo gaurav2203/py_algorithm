771efd3eaeda114e92c9d131265eb1b396b735ce
Gaurav <gauravtank2203@gmail.com>
2021-02-10 19:27:50 +0530

    insertion_sort

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        position= i
+        current_value= arr[i]
+        while position >0 and arr[position -1] > current_value:
+            arr[position]= arr[position-1]
+            position= position-1
+        arr[position]= current_value
+    return arr
